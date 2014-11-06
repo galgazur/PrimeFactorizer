ee0130348c4599327033c31bed41326971422712
Andreas Drangel <andreas.drangel@gmail.com>
2014-11-06 15:06:32 +0100

    Change some code in MainClass

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,49 +3,51 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class MainClass {
 
-	
-	
-	
-	public static void main(String[] args){
+	private final int ZERO_AMOUNT = 1;
+
+	public MainClass() {
 		String andreas = "9103090198";
 		String niklas = "9204123476";
-		
-		for (int i = 0; i < 80; i++) {
-			andreas+= "0";
-			niklas+= "0";
+
+		for (int i = 0; i < ZERO_AMOUNT; i++) {
+			andreas += "0";
+			niklas += "0";
 		}
-		
+
 		BigInteger andreasBase = new BigInteger(andreas);
 		BigInteger niklasBase = new BigInteger(niklas);
-		
+
 		QuadraticSieve qSieve = new QuadraticSieve();
-		
+
 		PrimeFinder p = new PrimeFinder();
 		BigInteger temp = null;
 		String result = "";
-		
+
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
 		Date date = new Date();
-		System.out.println(); //2014/08/06 15:59:48
+		System.out.println(); // 2014/08/06 15:59:48
 		String time = dateFormat.format(date);
-		
-		for(int i = 1; i<=100; i++){
+
+		for (int i = 1; i <= 100; i++) {
 			for (int j = 0; j < 2; j++) {
-				if(j==0){
-					temp = andreasBase.add(new BigInteger(""+i));
-				}else{
-					temp = niklasBase.add(new BigInteger(""+i));
+				if (j == 0) {
+					temp = andreasBase.add(new BigInteger("" + i));
+				} else {
+					temp = niklasBase.add(new BigInteger("" + i));
 				}
-				 
-				result = qSieve.factorize(p,temp);
-				
-				Logger.log(temp.toString(), result, time);	
+
+				result = qSieve.factorize(p, temp);
+
+				Logger.log(temp.toString(), result, time);
 			}
 		}
-		
 	}
-	
+
+	public static void main(String[] args) {
+		new MainClass();
+
+	}
+
 }
